226b90cfa5fa62dc9683685202444ed94b79dbe9
Shemar Lindie <shemarlindie@gmail.com>
2021-09-10 11:19:20 -0400

    Model field updates

--- django_api/api/skill/models.py
+++ django_api/api/skill/models.py
@@ -5,7 +5,7 @@ from django_api.utils.models import BaseModel
 
 class Project(BaseModel):
     name = models.CharField(max_length=100)
-    link = models.CharField(max_length=200, null=True)
+    link = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(max_length=500)
     visible = models.BooleanField(default=True)
 
@@ -14,9 +14,11 @@ class Project(BaseModel):
 
 
 class Skill(BaseModel):
-    name = models.CharField(max_length=100, null=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500, null=True, blank=True)
     projects = models.ManyToManyField(Project, related_name='skills')
     visible = models.BooleanField(default=True)
+    icon = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.name
